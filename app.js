936c1411161a98ae22f065a8dc3eaf1d8c1fa487
samvisser <samvisser04@gmail.com>
2024-11-08 11:15:21 -0500

    yo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { Client } = require('pg'); // Import PostgreSQL client
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Test route for DB connection and environment variables
+app.get('/test-db-connection', async (req, res) => {
+  const dbServer = process.env.DB_SERVER;
+  const dbPort = process.env.DB_PORT;
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASSWORD;
+  const dbDatabase = process.env.DB_DATABASE;
+
+  // Check if all required environment variables are available
+  if (!dbServer || !dbPort || !dbUser || !dbPassword || !dbDatabase) {
+    return res.status(500).send('Missing one or more environment variables');
+  }
+
+  // Create a PostgreSQL client and try to connect to the database
+  const client = new Client({
+    user: dbUser,
+    host: dbServer,
+    database: dbDatabase,
+    password: dbPassword,
+    port: dbPort,
+  });
+
+  try {
+    await client.connect();
+    res.send('Successfully connected to the database!');
+  } catch (err) {
+    res.status(500).send('Failed to connect to the database: ' + err.message);
+  } finally {
+    client.end();
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
